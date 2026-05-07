c6463506291e27b391c5b9f538caaabf88afd344
Sarveswaran S <sarveswaran.s.s.141@kalvium.community>
2026-05-07 21:05:43 +0530

    Initial React setup with Vite

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>QuickCart</h1>
+    </div>
+  );
+}
+
+export default App;
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
